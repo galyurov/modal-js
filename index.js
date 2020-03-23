db4861b6262d2f0d49bc7aa0f85b8fe4292a7580
galyurov <galyurov@gmail.com>
2020-03-23 17:25:48 +0200

    some edit and add closing, destroying modal

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-$.modal()
+const myModal = $.modal({
+	title: 'My Modal',
+	closable: true,
+	content: `
+		<h4>Modal is working</h4>
+		<p>Lorem ipsum dolor sit amet.</p>
+	`,
+	width: '400px'
+});
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,16 +1,17 @@
 function _createModal(options) {
+	const DEFAULT_WIDTH = '600px';
 	const modal = document.createElement('div');
 	modal.classList.add('vmodal');
-	modal.insertAdjacentHTML('afterbegin',`<div class="vmodal">
-      <div class="vmodal__overlay">
-         <div class="vmodal__window">
+	modal.insertAdjacentHTML('afterbegin',`
+	<div class="vmodal">
+      <div class="vmodal__overlay" data-close="true">
+         <div class="vmodal__window" style="width: ${options.width || DEFAULT_WIDTH}">
             <div class="vmodal__header">
-               <span class="vmodal__title">Modal title</span>
-               <span class="vmodal__close">&times;</span>
+               <span class="vmodal__title">${options.title || 'Title'}</span>
+               ${options.closable ? `<span class="vmodal__close" data-close="true">&times;</span>` : ''}
             </div>
             <div class="vmodal__body">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda aut debitis distinctio dolore eveniet iusto magnam magni maiores omnis porro provident, quod quos rem repellat repudiandae tempore! Doloribus, quaerat?</p>
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda aut debitis distinctio dolore eveniet iusto magnam magni maiores omnis porro provident, quod quos rem repellat repudiandae tempore! Doloribus, quaerat?</p>
+               ${options.content || ''}
             </div>
             <div class="vmodal__footer">
                <button>Ok</button>
@@ -18,11 +19,45 @@ function _createModal(options) {
             </div>
          </div>
       </div>
-   </div>`}
-$.modal = function (options) {
-	return {
-		open() {},
-		close(){},
-		destroy(){}
-	}
+    </div>`);
+	document.body.appendChild(modal);
+	return modal
 }
+$.modal = function (options) {
+	const animSpeed = 200;
+	const $modal = _createModal(options);
+	let closing = false;
+	let destroyed = false;
+	const modal = {
+		open() {
+			if(destroyed) {
+				return console.log('Modal is destroyed')
+			}
+			!closing && $modal.classList.add('open')
+		},
+		close(){
+			closing = true;
+			$modal.classList.remove('open');
+			$modal.classList.add('hide');
+			setTimeout(()=> {
+				$modal.classList.remove('hide');
+				closing = false
+			},animSpeed)
+		}
+	};
+	const listener = event => {
+		if (event.target.dataset.close) {
+			modal.close()
+		}
+	};
+
+	$modal.addEventListener('click', listener);
+
+	return Object.assign(modal, {
+		destroy() {
+			$modal.parentNode.removeChild($modal);
+			$modal.removeEventListener('click', listener);
+			destroyed = true
+		}
+	})
+};
